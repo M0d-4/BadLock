612384a782702742d7c1e39448579397da5c05b5
Mod4 <omarelfarok24135@gmail.com>
2026-05-26 01:43:35 +0400

    Update MainActivity.kt

--- app/src/main/java/com/dark/badlock/MainActivity.kt
+++ app/src/main/java/com/dark/badlock/MainActivity.kt
@@ -1289,7 +1289,7 @@ fun MainScreen(cacheManager: CacheManager) {
 
                                 Spacer(Modifier.height(16.dp))
                                 Text(
-                                    "Cool-Lock v1.8.1",
+                                    "Cool-Lock v1.8.3",
                                     color = appColors.textSecondary,
                                     fontSize = 12.sp,
                                     modifier = Modifier.align(Alignment.CenterHorizontally)
